2453d2977e10717f987bc36ddd36934d6de3cf7c
Scheww <llucas1996@gmail.com>
2021-12-18 12:57:56 -0300

    Add banner component

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,5 +1,6 @@
 import { useEthers } from "@usedapp/core";
 import { useTranslation } from "react-i18next";
+import Banner from "src/components/Banner/Banner";
 
 const HomePage = () => {
   const { account } = useEthers();
@@ -7,6 +8,11 @@ const HomePage = () => {
 
   return (
     <>
+      <Banner
+        title={"home.banner_title"}
+        subtitle={"home.banner_subtitle"}
+        image={'images/photos/homebanner.jpeg'}
+      />
       <h3>Landing Page</h3>
       Translations: {t("Test")}
       {account && <p>Account Connected: {account}</p>}
