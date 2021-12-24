2f3e151962a88320a6efb24a0792bfd356fa21c4
Scheww <llucas1996@gmail.com>
2021-12-23 21:52:14 -0300

    Update responsive

--- src/features/layouts/Layout.jsx
+++ src/features/layouts/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
     <>
       <Navbar/>
       <div className="flex flex-col md:flex-row">
-        <main className="main flex flex-col min-h-screen dark:bg-d-1 flex-no-wrap py-8 px-20 border-r-2 border-grey flex-1">
+        <main className="main flex flex-col min-h-screen dark:bg-d-1 flex-no-wrap px-5 py-5 md:px-20 md:py-8 border-r-2 border-grey flex-1">
           {children}
         </main>
 
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -8,34 +8,31 @@ const HomePage = () => {
   const { t } = useTranslation();
 
   return (
-    <>
+    <div>
       <Banner
         title={t("home.banner_title")}
         subtitle={t("home.banner_subtitle")}
         image={'images/photos/homebanner.jpeg'}
       />
-      <div className="w-full grid grid-cols-2 gap-2 mt-8">
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-2 mt-8">
         <HomeCardColored
           title={t("home.card.new_agro.title")}
           mainText={t("home.card.new_agro.number")}
           thirdText={t("home.card.new_agro.button_text")}
           onClickButton={console.log}
-          containerClasses="mr-2"
         />
         <HomeCardColored
           title={t("home.card.actives.title")}
           mainText={t("home.card.actives.number")}
           thirdText={t("home.card.actives.third_text")}
-          containerClasses="ml-2"
         />
       </div>
-      <div className="w-full grid grid-cols-2 gap-2 mt-8">
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-2 mt-8">
         <HomeCard
           title={t("home.card.nmilk.title")}
           description={t("home.card.nmilk.description")}
           thirdText={t("home.card.nmilk.third_text")}
           onClickButton={console.log}
-          containerClasses="mr-2"
           buttonText={t("home.card.nmilk.button_text")}
         />
         <HomeCardSecondary
@@ -43,14 +40,13 @@ const HomePage = () => {
           description={t("home.card.cows.description")}
           thirdText={t("home.card.cows.third_text")}
           onClickButton={console.log}
-          containerClasses="ml-2"
           buttonText={t("home.card.cows.button_text")}
           fourthText={t("home.card.cows.fourth_text")}
           fifthText={t("home.card.cows.fifth_text")}
           icon="icons/cow.svg"
         />
       </div>
-    </>
+    </div>
   );
 };
 
