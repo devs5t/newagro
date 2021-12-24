ba3abf37b5d625f00c832711e72a054b1ff553a2
Scheww <llucas1996@gmail.com>
2021-12-23 23:00:55 -0300

    Make white cards responsive

--- src/components/HomeCard/HomeCard.tsx
+++ src/components/HomeCard/HomeCard.tsx
@@ -22,14 +22,13 @@ const HomeCard: React.FC<HomeCardProps> =
 
   return (
     <div className={`min-h-280 flex justify-center items-center w-full rounded-lg border-green/[.5] border-2 shadow p-4 ${containerClasses}`}>
-      <div className={`h-full w-3/6 flex flex-col p-3  mt-3`}>
-        <h3 className="text-blue text-lg font-semibold mb-5">{title}</h3>
-        <p className="text-blue" >{description}</p>
+      <div className={`h-full w-3/6 flex flex-col md:p-3 md:mt-3`}>
+        <h3 className="text-blue font-semibold mb-3 md:mb-5 md:text-lg md:text-lg">{title}</h3>
+        <p className="text-blue text-tiny md:text-base">{description}</p>
       </div>
       <div className={`h-full w-3/6 flex flex-col justify-between px-3 items-end`}>
-        <h3 className="text-blue text-lg text-5xl mb-5 text-right">{thirdText}</h3>
-        <Button text={buttonText} onClick={onClickButton} extraClasses=" w-4/6 border-2 border-blue font-bold text-blue py-4 px-0" />
-
+        <h3 className="text-blue text-lg text-3xl mb-5 text-right md:text-5xl">{thirdText}</h3>
+        <Button text={buttonText} onClick={onClickButton} extraClasses="px-2 md:px-0 md:w-4/6 border-2 border-blue font-bold text-blue py-4 px-0" />
       </div>
     </div>
   );
--- src/components/HomeCard/HomeCardSecondary.tsx
+++ src/components/HomeCard/HomeCardSecondary.tsx
@@ -27,18 +27,18 @@ const HomeCardSecondary: React.FC<HomeCardSecondaryProps> = ({
 }) => {
   return (
     <div className={`min-h-280 flex flex-col w-full rounded-lg border-green/[.5] border-2 shadow ${containerClasses}`}>
-      <div className={`flex justify-center items-center w-full p-4`}>
-        <div className={`h-full w-3/6 flex flex-col p-3 mt-3`}>
-          <h3 className="text-blue text-lg font-semibold mb-5">{title}</h3>
-          <p className="text-blue">{description}</p>
+      <div className={`flex justify-center items-center w-full p-4 pb-0 md:pb-4`}>
+        <div className={`h-full w-3/6 flex flex-col md:p-3 md:mt-3`}>
+          <h3 className="text-blue font-semibold mb-3 md:mb-5 md:text-lg">{title}</h3>
+          <p className="text-blue text-tiny md:text-base">{description}</p>
         </div>
         <div className={`h-full w-3/6 flex flex-col justify-between px-3`}>
-          <h3 className="text-blue text-lg text-5xl mb-5 text-right">{thirdText}</h3>
+          <h3 className="text-blue text-lg text-3xl mb-5 md:text-5xl text-right">{thirdText}</h3>
         </div>
       </div>
-      <div className="flex justify-between items-center w-full border-green/[.25] border-t-2">
-        <div className="h-full w-3/6 flex flex-col justify-between px-7 p-4">
-          <h3 className="text-green font-semibold text-lg">{fourthText}</h3>
+      <div className="flex justify-between items-center w-full md:border-green/[.25] md:border-t-2">
+        <div className="h-full w-3/6 flex p-4 flex-col justify-evenly md:justify-betweenmd:px-7 md:p-4">
+          <h3 className="text-green font-semibold text-sm md:text-lg">{fourthText}</h3>
           <div className="flex">
             {icon && (
               <ReactSVG
@@ -46,13 +46,15 @@ const HomeCardSecondary: React.FC<HomeCardSecondaryProps> = ({
                 beforeInjection={(svg) => {
                   svg.classList.add('fill-green');
                   svg.classList.add('mr-4');
+                  svg.classList.add('text-sm');
+                  svg.classList.add('md:text-lg');
                 }}
               />
             )}
-            <p className="text-green font-semibold text-lg">{fifthText}</p>
+            <p className="text-green font-semibold text-sm md:text-lg">{fifthText}</p>
           </div>
         </div>
-        <div className="h-full w-2/6 flex flex-col justify-between px-7 p-4">
+        <div className="h-full w-3/6 flex flex-col justify-between md:px-7 p-4">
           <Button text={buttonText} onClick={onClickButton} extraClasses="border-2 border-blue font-bold text-blue py-4 px-0"/>
         </div>
       </div>
