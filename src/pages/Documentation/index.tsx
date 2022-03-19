37072b120220f3268eb8de6bec03a2dedae5d02e
Santiago Toscano <santiago.toscano@letflow.com.ar>
2022-03-18 23:32:56 -0300

    Fix home and investment values

--- src/pages/Documentation/index.tsx
+++ src/pages/Documentation/index.tsx
@@ -85,102 +85,104 @@ const Documentation: React.FC = () => {
   };
 
   return (
-    <div className="xl:py-8 2xl:px-56 xl:px-48">
-      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-        <Alert severity="error" onClose={handleClose}>
-          {t("error.generic")}
-        </Alert>
-      </Snackbar>
-      <Banner
-        title={t("docs.banner.title")}
-        subtitle={t("docs.banner.subtitle")}
-        image={'images/photos/bg_nmilk.jpeg'}
-        containerClasses={"p-4 md:p-4"}
-      />
-      <div className="w-full grid grid-cols-2 md:grid-cols-2 gap-2 mt-8 lg:px-16 xl:px-32 2xl:px-48 ">
-        <a
-          href="https://newagrocoin.gitbook.io/whitepaper"
-          target="_blank"
-          rel="noreferrer"
-          className="flex flex-row inline-block text-tiny px-2 leading-none border rounded-full md:px-0 w-full
-           border-1 border-blue text-blue py-2 justify-around pointer md:border-2 md:text-lg lg:px-4"
-        >
-          {t("docs.buttons.doc_tech")}
-          <ReactSVG
-            src={"icons/arrow.svg"}
-            beforeInjection={(svg) => {
-              svg.classList.add('fill-blue');
-              svg.classList.add('text-tiny');
-              svg.classList.add('lg:w-4');
-              svg.classList.add('lg:h-4');
-              svg.classList.add('-rotate-[135deg]');
-            }}
+    <div className="flex justify-center mt-8">
+      <div className="max-w-3xl w-full">
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert severity="error" onClose={handleClose}>
+            {t("error.generic")}
+          </Alert>
+        </Snackbar>
+        <Banner
+          title={t("docs.banner.title")}
+          subtitle={t("docs.banner.subtitle")}
+          image={'images/photos/bg_nmilk.jpeg'}
+          containerClasses={"p-4 md:p-4"}
+        />
+        <div className="w-full grid grid-cols-2 md:grid-cols-2 gap-2 mt-8 lg:px-16 xl:px-32 2xl:px-48 ">
+          <a
+            href="https://newagrocoin.gitbook.io/whitepaper"
+            target="_blank"
+            rel="noreferrer"
+            className="flex flex-row inline-block text-tiny px-2 leading-none border rounded-full md:px-0 w-full
+             border-1 border-blue text-blue py-2 justify-around pointer md:border-2 md:text-lg lg:px-4"
+          >
+            {t("docs.buttons.doc_tech")}
+            <ReactSVG
+              src={"icons/arrow.svg"}
+              beforeInjection={(svg) => {
+                svg.classList.add('fill-blue');
+                svg.classList.add('text-tiny');
+                svg.classList.add('lg:w-4');
+                svg.classList.add('lg:h-4');
+                svg.classList.add('-rotate-[135deg]');
+              }}
+            />
+          </a>
+          <a
+            href="https://newagro.com.ar"
+            target="_blank"
+            rel="noreferrer"
+            className="flex flex-row inline-block text-tiny px-2 leading-none border rounded-full md:px-0 w-full
+             border-1 border-blue text-blue py-2 justify-around pointer md:border-2 md:text-lg lg:px-4"
+          >
+            {t("docs.buttons.page_inst")}
+            <ReactSVG
+              src={"icons/arrow.svg"}
+              beforeInjection={(svg) => {
+                svg.classList.add('fill-blue');
+                svg.classList.add('text-tiny');
+                svg.classList.add('lg:w-4');
+                svg.classList.add('lg:h-4');
+                svg.classList.add('-rotate-[135deg]');
+              }}
+            />
+          </a>
+        </div>
+        <div className="w-full justify-center flex my-8">
+          <Tabs
+            tabs={[
+              {name: 'New Milk', selected: true},
+              {name: 'New Beef', disabled: true},
+              {name: 'New Land', disabled: true}
+            ]}
+            containerClass="max-w-xl"
           />
-        </a>
-        <a
-          href="https://newagro.com.ar"
-          target="_blank"
-          rel="noreferrer"
-          className="flex flex-row inline-block text-tiny px-2 leading-none border rounded-full md:px-0 w-full
-           border-1 border-blue text-blue py-2 justify-around pointer md:border-2 md:text-lg lg:px-4"
-        >
-          {t("docs.buttons.page_inst")}
-          <ReactSVG
-            src={"icons/arrow.svg"}
-            beforeInjection={(svg) => {
-              svg.classList.add('fill-blue');
-              svg.classList.add('text-tiny');
-              svg.classList.add('lg:w-4');
-              svg.classList.add('lg:h-4');
-              svg.classList.add('-rotate-[135deg]');
+        </div>
+        <div className="w-full grid grid-cols-1 mt-8">
+          <DocumentationCard
+            title={t("docs.cards.card1_title")}
+            subtitle={t("docs.cards.card1_subtitle")}
+            linkText={t("docs.cards.see_docs")}
+            link={"#"}
+            signIn={() => {
+              if (!auth?.isSignedIn.get()) {
+                auth?.signIn();
+              }
             }}
+            onClick={() => auth?.isSignedIn.get() && getFiles()}
+            component={<SearchList listItems={files} onDownload={onDownload} />}
           />
-        </a>
-      </div>
-      <div className="w-full justify-center flex my-8">
-        <Tabs
-          tabs={[
-            {name: 'New Milk', selected: true},
-            {name: 'New Beef', disabled: true},
-            {name: 'New Land', disabled: true}
-          ]}
-          containerClass="max-w-xl"
-        />
-      </div>
-      <div className="w-full grid grid-cols-1 mt-8">
-        <DocumentationCard
-          title={t("docs.cards.card1_title")}
-          subtitle={t("docs.cards.card1_subtitle")}
-          linkText={t("docs.cards.see_docs")}
-          link={"#"}
-          signIn={() => {
-            if (!auth?.isSignedIn.get()) {
-              auth?.signIn();
-            }
-          }}
-          onClick={() => auth?.isSignedIn.get() && getFiles()}
-          component={<SearchList listItems={files} onDownload={onDownload} />}
-        />
-        <br />
-        <DocumentationCard
-          title={t("docs.cards.card2_title")}
-          component={<Map />}
-        />
-        <br />
-        <DocumentationCard
-          title={t("docs.cards.card3_title")}
-          subtitle={t("docs.cards.card3_subtitle")}
-          linkText={t("docs.cards.see_docs")}
-          link={"#"}
-        />
-        <br />
-        <DocumentationCard
-          title={t("docs.cards.card4_title")}
-          subtitle={t("docs.cards.card4_subtitle")}
-          linkText={t("docs.cards.see_docs")}
-          link={"#"}
-        />
-        <br />
+          <br />
+          <DocumentationCard
+            title={t("docs.cards.card2_title")}
+            component={<Map />}
+          />
+          <br />
+          <DocumentationCard
+            title={t("docs.cards.card3_title")}
+            subtitle={t("docs.cards.card3_subtitle")}
+            linkText={t("docs.cards.see_docs")}
+            link={"#"}
+          />
+          <br />
+          <DocumentationCard
+            title={t("docs.cards.card4_title")}
+            subtitle={t("docs.cards.card4_subtitle")}
+            linkText={t("docs.cards.see_docs")}
+            link={"#"}
+          />
+          <br />
+        </div>
       </div>
     </div>
   );
