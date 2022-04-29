58464ebab060b0529bed0b41892cf6adb73a2135
santiagotoscano <santiago.toscano@letflow.com.ar>
2022-04-29 00:55:42 -0300

    Fix icons in android

--- build-apps.sh
+++ build-apps.sh
@@ -3,14 +3,18 @@
 rm -rf dist
 rm -rf ios
 rm -rf android
+rm -rf resources/ios/icon
+rm -rf resources/ios/splash
+rm -rf resources/android/icon
+rm -rf resources/android/splash
 
 yarn run build
-yarn resources
+capacitor-resources -p android,ios
 
 npx cap add ios
 cordova-res ios --skip-config --copy
-npx cap run ios
+#npx cap run ios
 
 npx cap add android
 cordova-res android --skip-config --copy
-npx cap run android
+#npx cap run android
