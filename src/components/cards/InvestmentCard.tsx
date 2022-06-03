32dcb697df238641f18be2d7cf40d98c69193b72
ignaciocanosa <ignaciocanosa@gmail.com>
2022-06-03 10:24:18 -0300

    Update InvestmentCard.tsx

--- src/components/cards/InvestmentCard.tsx
+++ src/components/cards/InvestmentCard.tsx
@@ -253,7 +253,7 @@ const InvestmentCard: React.FC<InvestmentCardProps> = ({
               <CountUp
                 className="flex justify-center text-center text-blue text-2xl"
                 end={totalAssets}
-                prefix="USD "
+                prefix="USDT "
                 decimals={2}
                 separator=","
                 decimal="."
