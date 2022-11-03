4f91ec486fc21eb1b6d90ce80af14e0fd04cf026
Gonzalo Scannone <gonzaloscannone@gmail.com>
2022-11-03 18:42:06 -0300

    test contracts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -76,107 +76,133 @@ export default {
 
   //---------------------------Legacy template---------------------------------
   masterChef: {
+    97: "",
     56: "0x38ce1EB7D3b0fe09D678C93972fB73Bf2e61a48C",
     42: "0x38ce1EB7D3b0fe09D678C93972fB73Bf2e61a48C",
   },
   cake: {
+    42: "",
     97: "",
     56: "0xD9A88f9b7101046786490bAF433f0f6aB3D753E2",
   },
   syrup: {
+    42: "",
     97: "",
     56: "",
   },
   bbqIFO: {
+    42: "",
     97: "",
     56: "0x50D3CcF386e389Cf7f83eF7F79c0eAd9e2a51584",
   },
   sousChef: {
+    42: "",
     97: "",
     56: "",
   },
   wbnb: {
     97: "0xae13d989dac2f0debff460ac112a837c89baa7cd",
+    42: "",
     56: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
   },
   lottery: {
+    42: "",
     97: "",
     56: "0x42c559F10AfA83e5189568e66505aEf077960E6B",
   },
   lotteryNFT: {
+    42: "",
     97: "",
     56: "0x17780e8dB46f02c8bCb0ccFF64E696AbefF0a931",
   },
   mulltiCall: {
+    97: "",
     56: "0x38ce767d81de3940CFa5020B55af1A400ED4F657",
-    97: "0x38ce767d81de3940CFa5020B55af1A400ED4F657",
+    42: "0x38ce767d81de3940CFa5020B55af1A400ED4F657",
   },
   busd: {
-    56: "0xe9e7cea3dedca5984780bafc599bd69add087d56",
     97: "",
+    56: "0xe9e7cea3dedca5984780bafc599bd69add087d56",
+    42: "",
   },
   ust: {
-    56: "0x23396cF899Ca06c4472205fC903bDB4de249D6fC",
     97: "",
+    56: "0x23396cF899Ca06c4472205fC903bDB4de249D6fC",
+    42: "",
   },
   pancakeProfile: {
     56: "0xDf4dBf6536201370F95e06A0F8a7a70fE40E388a",
     97: "0x4B683C7E13B6d5D7fd1FeA9530F451954c1A7c8A",
+    42: "",
   },
   pancakeRabbits: {
     56: "0xDf7952B35f24aCF7fC0487D01c8d5690a60DBa07",
     97: "0x60935F36e4631F73f0f407e68642144e07aC7f5E",
+    42: "",
   },
   bunnyFactory: {
     56: "0xfa249Caa1D16f75fa159F7DFBAc0cC5EaB48CeFf",
     97: "0x707CBF373175fdB601D34eeBF2Cf665d08f01148",
+    42: "",
   },
   eth: {
     56: "0x2170ed0880ac9a755fd29b2688956bd959f933f8",
     97: "",
+    42: "",
   },
   cakecake: {
     97: "",
     56: "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82",
+    42: "",
   },
   claimRefund: {
     56: "0xE7e53A7e9E3Cf6b840f167eF69519175c497e149",
     97: "",
+    42: "",
   },
   pointCenterIfo: {
     56: "0x3C6919b132462C1FEc572c6300E83191f4F0012a",
     97: "0xd2Ac1B1728Bb1C11ae02AB6e75B76Ae41A2997e3",
+    42: "",
   },
   bunnySpecial: {
     56: "0xFee8A195570a18461146F401d6033f5ab3380849",
     97: "0x7b7b1583De1DeB32Ce6605F6deEbF24A0671c17C",
+    42: "",
   },
   defiLaunch: {
     56: "0xd7d23d89c510f306e4037385ebbefc6dd6c41e61",
     97: "",
+    42: "",
   },
   defiLaunchBusdLp: {
     56: "0xfb663c6405acfe4a013f5c9cd241f38d0d0b7123",
     97: "",
+    42: "",
   },
   bikini: {
     56: "0xC865a1dFB8717F53261a935D5C2577fDA7523C55",
     97: "",
+    42: "",
   },
   bikiniBusdLp: {
     56: "0xfb663c6405acfe4a013f5c9cd241f38d0d0b7123",
     97: "",
+    42: "",
   },
   bikiniBnbLp: {
     56: "0xeb61d3be592f320fc69ebf4b3498e9f8b5f89c67",
     97: "",
+    42: "",
   },
   referral: {
     56: "0x68AC83fcDC1f2f76e6F14bded3eD04334d74d358",
     97: "",
+    42: "",
   },
   zap: {
     56: "0xe1A6625004726Cbcc693a34FefF9283E6433934f",
     97: "",
+    42: "",
   },
 };
